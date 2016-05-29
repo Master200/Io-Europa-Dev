7796239adbdb9e99d2bd24d1fab5ef2fe37eb6a9
Zentrex1 <Ishana02@yahoo.com>
2016-05-29 14:37:00 -0500

    Add files via upload

---
+++ messanger.bat
@@ -0,0 +1,27 @@
+@echo off
+color 0b
+
+title Chat Messenger
+
+echo Hi, welcome to the chat program!
+
+echo.
+
+set /p username=Enter Your Name:
+
+echo %username% has joined! >> join.dat
+
+:message
+
+
+set /p message=Say:
+
+goto send
+
+:send
+
+echo %username%: %message% (%time%)>> log.dat
+
+goto message
+
+
---
+++ receive.bat
@@ -0,0 +1,17 @@
+@echo off
+color 0b
+
+:read
+
+cls
+
+type join.dat
+
+echo.
+
+type log.dat
+
+echo.
+
+goto read
+
